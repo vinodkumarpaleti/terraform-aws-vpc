8c84ebb3125cee710905e2fc069c1c840b693cdc
vinodkumar <kumarvinod.paleti@gmail.com>
2023-12-06 21:03:01 +0530

    new files are added

--- main.tf
+++ main.tf
@@ -2,9 +2,114 @@ resource "aws_vpc" "main" {
   cidr_block       = var.cidr_block
   enable_dns_hostnames = var.enable_dns_hostnames
   enable_dns_support = var.enable_dns_support
-  
+
     tags = merge(
     var.common_tags,
     var.vpc_tags
   )
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags,
+    var.igw_tags
+  )
+  
+}
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidr[count.index]
+  availability_zone = var.azs[count.index]
+  tags = merge(
+    var.common_tags,
+    {
+        Name = var.public_subnet_names[count.index]
+    }
+  )
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidr[count.index]
+  availability_zone = var.azs[count.index]
+  tags = merge(
+    var.common_tags,
+    {
+        Name = var.private_subnet_names[count.index]
+    }
+  )
+}
+
+resource "aws_subnet" "database" {
+  count = length(var.database_subnet_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.database_subnet_cidr[count.index]
+  availability_zone = var.azs[count.index]
+  tags = merge(
+    var.common_tags,
+    {
+        Name = var.database_subnet_names[count.index]
+    }
+  )
+}
+
+resource "aws_route" "public" {
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.main.id
+  depends_on  = [aws_route_table.public]
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags,
+    var.public_route_table_tags
+  )
+}
+
+# associate public route table with public subnets
+# public-route-table --> public-1a subnet
+# public-route-table --> public-1b subnet
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidr)
+  subnet_id      = element(aws_subnet.public[*].id, count.index)
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags,
+    var.private_route_table_tags
+  )
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnet_cidr)
+  subnet_id      = element(aws_subnet.private[*].id, count.index)
+  route_table_id = aws_route_table.private.id
+}
+
+
+resource "aws_route_table" "database" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags,
+    var.database_route_table_tags
+  )
+}
+
+resource "aws_route_table_association" "database" {
+  count = length(var.database_subnet_cidr)
+  subnet_id      = element(aws_subnet.database[*].id, count.index)
+  route_table_id = aws_route_table.database.id
 }
--- variables.tf
+++ variables.tf
@@ -19,3 +19,52 @@ variable "vpc_tags" {
   default = {} # this means tags is optional
   type = map
 }
+
+variable "igw_tags" {
+  default = {}
+}
+
+variable "public_subnet_cidr" {
+  
+  
+}
+
+variable "public_subnet_names" {
+  
+  
+}
+
+variable "azs" {
+  
+}
+
+variable "private_subnet_cidr" {
+  
+  
+}
+
+variable "private_subnet_names" {
+  
+  
+}
+
+variable "database_subnet_cidr" {
+  
+  
+}
+
+variable "database_subnet_names" {
+  
+  
+}
+variable "public_route_table_tags" {
+  default = {}
+}
+
+variable "private_route_table_tags" {
+  
+}
+
+variable "database_route_table_tags" {
+  
+}
